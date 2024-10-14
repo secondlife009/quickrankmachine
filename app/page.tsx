3f101bd263e632e8d04ca54a38cd83caec3c52e5
Second Life <secondlife986@gmail.com>
2024-10-14 10:18:43 +0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import CallToAction from "@/components/landing/CallToAction";
+import Features from "@/components/landing/Features";
+import Hero from "@/components/landing/Hero";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Features />
+      <CallToAction />
+    </main>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,134 @@
+import { FaYoutube } from "react-icons/fa";
+import { MdOutlineSubtitles } from "react-icons/md";
+import { PiSlideshow } from "react-icons/pi";
+import { MdNotes } from "react-icons/md";
+import { FaRobot } from "react-icons/fa";
+import { PiMicrosoftPowerpointLogoFill } from "react-icons/pi";
+import { MdBrandingWatermark } from "react-icons/md";
+
+const siteName = "YouTubeToSlides";
+
+const features = [
+  {
+    id: 1,
+    title: "YouTube Video Input",
+    description:
+      "Simply paste the URL of the YouTube video you want to convert into a Powerpoint presentation.",
+    icon: FaYoutube,
+  },
+  {
+    id: 2,
+    title: "Automatic Transcription",
+    description:
+      "We automatically convert the video's audio into text using speech-to-text technology.",
+    icon: MdOutlineSubtitles,
+  },
+  {
+    id: 3,
+    title: "Slide Content Generation",
+    description:
+      "We automatically generate slide titles and content based on the video's transcript, extracting key points and summarizing information.",
+    icon: PiSlideshow,
+  },
+  {
+    id: 4,
+    title: "Speaker Notes",
+    description:
+      "Automatic speaker note generation to help presenters prepare for their presentation.",
+    icon: MdNotes,
+  },
+  {
+    id: 5,
+    title: "Cleanse and Format",
+    description:
+      "95% of the time, our AI removes all references to the presentation coming from a YouTube video.",
+    icon: FaRobot,
+  },
+  {
+    id: 6,
+    title: "Export to PowerPoint",
+    description:
+      "Export the generated slides to a PowerPoint file for easy access and presentation.",
+    icon: PiMicrosoftPowerpointLogoFill,
+  },
+  {
+    id: 7,
+    title: "Your branding",
+    description:
+      "Tell us about the colours of your brand and we will make sure your slides match your brand.",
+    icon: MdBrandingWatermark,
+  },
+  {
+    id: 8,
+    title: "Save time and effort",
+    description:
+      "Automatically generate PowerPoint presentations from YouTube videos so that you can focus on other things to help your students learn",
+    icon: FaYoutube,
+  },
+];
+
+export const tips = [
+  {
+    icon: "🔗",
+    description:
+      "Make sure to enter a valid YouTube video URL. The video should be an educational video such as a tutorial.",
+  },
+  {
+    icon: "🔒",
+    description:
+      "Ensure that the video is not private and is no longer than 10 minutes. Longer videos won't be processed.",
+  },
+  {
+    icon: "🔊",
+    description:
+      "The video should have clear audio and be in English. This will help us generate accurate slides for you.",
+  },
+];
+
+export const examples = [
+  {
+    title: "Solving Linear Equations: Isolating Variables for Function Analysis",
+    description: "Learn to isolate variables in linear equations, derive functions, and apply practical insights for problem-solving.",
+    createdAt: "2024-09-05",
+    link: "https://utfs.io/f/a5eefbee-f35e-40d0-a789-ec0a98cd70cd-8zz4vy.pptx",
+  },
+  {
+    title: "Solving Linear Equations: Isolating Variables for Function Analysis",
+    description: "Learn to isolate variables in linear equations, derive functions, and apply practical insights for problem-solving.",
+    createdAt: "2024-09-05",
+    link: "https://utfs.io/f/a5eefbee-f35e-40d0-a789-ec0a98cd70cd-8zz4vy.pptx",
+  },
+  {
+    title: "Solving Linear Equations: Isolating Variables for Function Analysis",
+    description: "Learn to isolate variables in linear equations, derive functions, and apply practical insights for problem-solving.",
+    createdAt: "2024-09-05",
+    link: "https://utfs.io/f/a5eefbee-f35e-40d0-a789-ec0a98cd70cd-8zz4vy.pptx",
+  },
+  {
+    title: "Solving Linear Equations: Isolating Variables for Function Analysis",
+    description: "Learn to isolate variables in linear equations, derive functions, and apply practical insights for problem-solving.",
+    createdAt: "2024-09-05",
+    link: "https://utfs.io/f/a5eefbee-f35e-40d0-a789-ec0a98cd70cd-8zz4vy.pptx",
+  },
+  {
+    title: "Solving Linear Equations: Isolating Variables for Function Analysis",
+    description: "Learn to isolate variables in linear equations, derive functions, and apply practical insights for problem-solving.",
+    createdAt: "2024-09-05",
+    link: "https://utfs.io/f/a5eefbee-f35e-40d0-a789-ec0a98cd70cd-8zz4vy.pptx",
+  },
+  {
+    title: "Solving Linear Equations: Isolating Variables for Function Analysis",
+    description: "Learn to isolate variables in linear equations, derive functions, and apply practical insights for problem-solving.",
+    createdAt: "2024-09-05",
+    link: "https://utfs.io/f/a5eefbee-f35e-40d0-a789-ec0a98cd70cd-8zz4vy.pptx",
+  },
+  {
+    title: "Solving Linear Equations: Isolating Variables for Function Analysis",
+    description: "Learn to isolate variables in linear equations, derive functions, and apply practical insights for problem-solving.",
+    createdAt: "2024-09-05",
+    link: "https://utfs.io/f/a5eefbee-f35e-40d0-a789-ec0a98cd70cd-8zz4vy.pptx",
+  },
+
+]
+
+export { features, siteName };
